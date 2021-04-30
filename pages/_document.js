ecbef28a75fa03085a48e99589b5f5d1a13c1ee2
raphaelmelo <contatowbm@gmail.com>
2021-04-30 08:59:05 -0300

    Update dependencie styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
 import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-
+import { ServerStyleSheets } from '@material-ui/styles';
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
+    const materialSheets = new ServerStyleSheets()
+    const styledComponentsSheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
@@ -11,6 +13,10 @@ export default class MyDocument extends Document {
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props =>
+            styledComponentsSheet.collectStyles(materialSheets
+              .collect(<App {...props} />)),
+
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -20,11 +26,14 @@ export default class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            {materialSheets.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
           </>
         ),
       }
     } finally {
       sheet.seal()
+      styledComponentsSheet.seal()
     }
   }
 }
